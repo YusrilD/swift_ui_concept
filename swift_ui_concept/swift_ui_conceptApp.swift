4fdb0bc56cddbea5990d18f7b0374a375fc578f1
YusrilD <yusrildewantara2@gmail.com>
2026-01-10 14:47:49 +0700

    Initial Commit

---
+++ swift_ui_concept/swift_ui_conceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_conceptApp.swift
+//  swift_ui_concept
+//
+//  Created by Triv on 10/01/26.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_conceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift_ui_conceptTests/swift_ui_conceptTests.swift
@@ -0,0 +1,16 @@
+//
+//  swift_ui_conceptTests.swift
+//  swift_ui_conceptTests
+//
+//  Created by Triv on 10/01/26.
+//
+
+import Testing
+
+struct swift_ui_conceptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
