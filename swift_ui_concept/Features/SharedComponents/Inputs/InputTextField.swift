c4a7f2a867c8045995364818c39ca04efdf065ef
YusrilD <yusrildewantara2@gmail.com>
2026-01-13 15:02:14 +0700

    add InputTextField Component

--- swift_ui_concept/Features/SharedComponents/Inputs/InputTextField.swift
+++ swift_ui_concept/Features/SharedComponents/Inputs/InputTextField.swift
@@ -5,3 +5,21 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct InputTextField: View {
+    let placeholder: String
+    @Binding var text: String
+    var isSecure: Bool = false
+
+    var body: some View {
+        Group {
+            if isSecure {
+                SecureField(placeholder, text: $text)
+            } else {
+                TextField(placeholder, text: $text)
+            }
+        }
+        .textFieldStyle(.roundedBorder)
+    }
+}
