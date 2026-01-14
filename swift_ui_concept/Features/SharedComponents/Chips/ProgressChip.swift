1ef32395f8b7d0525faf8780b8443b2d953a280f
YusrilD <yusrildewantara2@gmail.com>
2026-01-14 21:33:48 +0700

    add progress chip

--- swift_ui_concept/Features/SharedComponents/Chips/ProgressChip.swift
+++ swift_ui_concept/Features/SharedComponents/Chips/ProgressChip.swift
@@ -5,3 +5,23 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct ProgressChip: View {
+    let title: String
+    let progress: Double
+
+    var body: some View {
+        HStack {
+            Text(title)
+            ProgressView(value: progress)
+                .frame(width: 60)
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 6)
+        .background(Color.blue.opacity(0.1))
+        .clipShape(Capsule())
+    }
+}
+
+
