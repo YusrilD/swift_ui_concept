9155aa139b85b6bcc357b6ae74e059904a7d44cb
YusrilD <yusrildewantara2@gmail.com>
2026-01-14 21:32:12 +0700

    add identifiable

--- swift_ui_concept/Features/SharedComponents/Graphs/LineGraph.swift
+++ swift_ui_concept/Features/SharedComponents/Graphs/LineGraph.swift
@@ -11,19 +11,6 @@ import Charts
 
 struct LineGraph: View {
 
-    struct MonthlyHoursOfSunshine {
-        var date: Date
-        var hoursOfSunshine: Double
-
-
-        init(month: Int, hoursOfSunshine: Double) {
-            let calendar = Calendar.autoupdatingCurrent
-            self.date = calendar.date(from: DateComponents(year: 2020, month: month))!
-            self.hoursOfSunshine = hoursOfSunshine
-        }
-    }
-
-
     var data: [MonthlyHoursOfSunshine] = [
         MonthlyHoursOfSunshine(month: 1, hoursOfSunshine: 74),
         MonthlyHoursOfSunshine(month: 2, hoursOfSunshine: 99),
@@ -34,27 +21,27 @@ struct LineGraph: View {
 
     var body: some View {
         Chart(data) {
-                LineMark(
-                    x: .value("Month", $0.date),
-                    y: .value("Hours of Sunshine", $0.hoursOfSunshine)
-                )
-            }
+            LineMark(
+                x: .value("Month", $0.date),
+                y: .value("Hours of Sunshine", $0.hoursOfSunshine)
+            )
+        }
     }
 }
 
-//struct MonthlyHoursOfSunshine: Identifiable {
-//    var id = UUID()
-//    
-//    var date: Date
-//    var hoursOfSunshine: Double
-//
-//
-//    init(month: Int, hoursOfSunshine: Double) {
-//        let calendar = Calendar.autoupdatingCurrent
-//        self.date = calendar.date(from: DateComponents(year: 2020, month: month))!
-//        self.hoursOfSunshine = hoursOfSunshine
-//    }
-//}
+struct MonthlyHoursOfSunshine: Identifiable {
+    var id = UUID()
+    
+    var date: Date
+    var hoursOfSunshine: Double
+
+
+    init(month: Int, hoursOfSunshine: Double) {
+        let calendar = Calendar.autoupdatingCurrent
+        self.date = calendar.date(from: DateComponents(year: 2020, month: month))!
+        self.hoursOfSunshine = hoursOfSunshine
+    }
+}
 
 #Preview {
     LineGraph()
