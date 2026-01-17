7ea0edba8904c96aa546582f934197a383285790
YusrilD <yusrildewantara2@gmail.com>
2026-01-17 17:26:22 +0700

    add coin network card

--- swift_ui_concept/Features/SharedComponents/Cards/CoinNetworkCard.swift
+++ swift_ui_concept/Features/SharedComponents/Cards/CoinNetworkCard.swift
@@ -5,3 +5,20 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct CoinNetworkCard: View {
+    let networkName: String
+    let fee: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(networkName).bold()
+            Text("Fee: \(fee)")
+                .font(.caption)
+        }
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(12)
+    }
+}
