b341a50daf5ebcafa86919d977c62877294d8944
YusrilD <yusrildewantara2@gmail.com>
2026-01-17 17:28:29 +0700

    add Coin Card

--- swift_ui_concept/Features/SharedComponents/Cards/CoinCard.swift
+++ swift_ui_concept/Features/SharedComponents/Cards/CoinCard.swift
@@ -5,3 +5,20 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct CoinCard: View {
+    let symbol: String
+    let price: String
+
+    var body: some View {
+        HStack {
+            Text(symbol).bold()
+            Spacer()
+            Text(price)
+        }
+        .padding()
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(12)
+    }
+}
