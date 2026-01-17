f1a4ce79b502a2fa7c16af0f60ec5d337ef44b59
YusrilD <yusrildewantara2@gmail.com>
2026-01-17 17:30:23 +0700

    add recipientframe

--- swift_ui_concept/Features/SharedComponents/Cards/RecipientFrame.swift
+++ swift_ui_concept/Features/SharedComponents/Cards/RecipientFrame.swift
@@ -5,3 +5,23 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct RecipientFrame: View {
+    let name: String
+    let address: String
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(name).bold()
+            Text(address)
+                .font(.caption)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.gray.opacity(0.3))
+        )
+    }
+}
