d63cc0afe3015b791cc51b65a12f484fe16db4fd
YusrilD <yusrildewantara2@gmail.com>
2026-01-17 17:32:46 +0700

    add coin slider

--- swift_ui_concept/Features/SharedComponents/Sliders/CoinSlider.swift
+++ swift_ui_concept/Features/SharedComponents/Sliders/CoinSlider.swift
@@ -5,3 +5,13 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct CoinSlider: View {
+    @Binding var value: Double
+    let range: ClosedRange<Double>
+
+    var body: some View {
+        Slider(value: $value, in: range)
+    }
+}
