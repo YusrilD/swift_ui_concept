8e9e9e9f4625feb6d8c9d51fde9fc0452295bdf0
YusrilD <yusrildewantara2@gmail.com>
2026-01-17 17:17:27 +0700

    add App Button

--- swift_ui_concept/Features/SharedComponents/Buttons/AppButton.swift
+++ swift_ui_concept/Features/SharedComponents/Buttons/AppButton.swift
@@ -5,3 +5,22 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct AppButton: View {
+    let title: String
+    let action: () -> Void
+    var isDisabled: Bool = false
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(isDisabled ? Color.gray : Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+        }
+        .disabled(isDisabled)
+    }
+}
