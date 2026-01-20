117c375a5fef6ce8ed773f07be89886113b8b367
YusrilD <yusrildewantara2@gmail.com>
2026-01-20 11:28:52 +0700

    add theme app-spacing

--- swift_ui_concept/Core/Theme/AppSpacing.swift
+++ swift_ui_concept/Core/Theme/AppSpacing.swift
@@ -5,3 +5,11 @@
 //  Created by Triv on 20/01/26.
 //
 
+import SwiftUI
+
+enum AppSpacing {
+    static let xs: CGFloat = 8
+    static let sm: CGFloat = 12
+    static let md: CGFloat = 16
+    static let lg: CGFloat = 24
+}
