8967b586cf2460422f14156b78ab904dfd24fa25
YusrilD <yusrildewantara2@gmail.com>
2026-01-20 11:27:59 +0700

    add theme app font

--- swift_ui_concept/Core/Theme/AppFont.swift
+++ swift_ui_concept/Core/Theme/AppFont.swift
@@ -5,3 +5,10 @@
 //  Created by Triv on 20/01/26.
 //
 
+import SwiftUI
+
+enum AppFont {
+    static let body = Font.system(size: 16)
+    static let title = Font.system(size: 20, weight: .semibold)
+    static let caption = Font.system(size: 12)
+}
