892abf715fb85c53636635da3004f759f7b640a9
YusrilD <yusrildewantara2@gmail.com>
2026-01-21 22:17:16 +0700

    add selector time frame reusable ui

--- swift_ui_concept/Features/SharedComponents/Graphs/SelectorTimeFrame.swift
+++ swift_ui_concept/Features/SharedComponents/Graphs/SelectorTimeFrame.swift
@@ -5,3 +5,24 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct SelectorTimeFrame: View {
+    let options: [String]
+    @Binding var selected: String
+
+    var body: some View {
+        HStack {
+            ForEach(options, id: \.self) { option in
+                Button(option) {
+                    selected = option
+                }
+                .padding(.horizontal, 12)
+                .padding(.vertical, 6)
+                .background(selected == option ? Color.blue : Color.gray.opacity(0.2))
+                .foregroundColor(.white)
+                .cornerRadius(8)
+            }
+        }
+    }
+}
