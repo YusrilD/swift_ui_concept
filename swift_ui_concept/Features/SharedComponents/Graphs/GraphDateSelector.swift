e1bc8a2113249fe6535d80fbb451f190f761669f
YusrilD <yusrildewantara2@gmail.com>
2026-01-21 22:16:30 +0700

    add graph date selector

--- swift_ui_concept/Features/SharedComponents/Graphs/GraphDateSelector.swift
+++ swift_ui_concept/Features/SharedComponents/Graphs/GraphDateSelector.swift
@@ -5,3 +5,13 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct GraphDateSelector: View {
+    @Binding var date: Date
+
+    var body: some View {
+        DatePicker("", selection: $date, displayedComponents: .date)
+            .labelsHidden()
+    }
+}
