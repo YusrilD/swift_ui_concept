d577f7317ee648e76698c45ce0dd30e9420a1454
YusrilD <yusrildewantara2@gmail.com>
2026-01-21 22:17:51 +0700

    add search input reusable ui

--- swift_ui_concept/Features/SharedComponents/Inputs/SearchInput.swift
+++ swift_ui_concept/Features/SharedComponents/Inputs/SearchInput.swift
@@ -5,3 +5,18 @@
 //  Created by Triv on 11/01/26.
 //
 
+import SwiftUI
+
+struct SearchInput: View {
+    @Binding var text: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            TextField("Search...", text: $text)
+        }
+        .padding(10)
+        .background(Color.gray.opacity(0.1))
+        .cornerRadius(10)
+    }
+}
