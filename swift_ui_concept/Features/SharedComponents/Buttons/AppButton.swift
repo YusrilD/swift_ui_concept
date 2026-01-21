4ed33e6e1c5f5241429eef007087e956df7b777b
YusrilD <yusrildewantara2@gmail.com>
2026-01-21 22:14:29 +0700

    add disable button in AppButton class

--- swift_ui_concept/Features/SharedComponents/Buttons/AppButton.swift
+++ swift_ui_concept/Features/SharedComponents/Buttons/AppButton.swift
@@ -28,8 +28,31 @@ struct AppButton: View {
     }
 }
 
+struct AppButtonDisable: View {
+    let title: String
+    let action: () -> Void
+    var isDisabled: Bool = false
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.system(size: 24.0, weight: .bold))
+                .frame(maxWidth: .infinity)
+                .padding(.vertical)
+                .foregroundStyle(.white)
+        }
+        .background(isDisabled ? .gray : .disableButton)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20.0).stroke(.buttonBorder, lineWidth: 2.0)
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 20.0))
+        .padding(.horizontal)
+        .disabled(isDisabled)
+    }
+}
+
 #Preview {
-    AppButton(
+    AppButtonDisable(
         title: "Next",
         action: {},
         isDisabled: false
